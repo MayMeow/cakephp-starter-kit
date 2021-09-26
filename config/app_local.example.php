a92e251852b17e0382fbb5f5f2bd8b8ca43f23fb
アマヤ <may@maymeow.moe>
2021-09-26 16:24:34 +0000

    Update bin/cake.bat, config/app_local.example.php files

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -25,7 +25,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', null),
+        'salt' => env('SECURITY_SALT', '__SALT__'),
     ],
 
     /*
